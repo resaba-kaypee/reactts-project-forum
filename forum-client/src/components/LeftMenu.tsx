56eefb0c7ca7b8bfedec3b1939ec2507548ba04c
kaypee <resaba.kaypee@gmail.com>
2021-03-28 22:27:05 +0700

    client-simulate fake network call

--- forum-client/src/components/LeftMenu.tsx
+++ forum-client/src/components/LeftMenu.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { getCategories } from "../services/DataService";
+import Category from "../models/Category";
+import { useWindowDimensions } from "../hooks/useWindowDimensions";
 
 const LeftMenu = () => {
-  return <div className="leftmenu">Left Menu</div>;
+  const { width } = useWindowDimensions();
+
+  const [categories, setCategories] = useState<JSX.Element>(
+    <div>Left Menu</div>
+  );
+
+  useEffect(() => {
+    getCategories()
+      .then((categoriesArr: Array<Category>) => {
+        const cats = categoriesArr.map((category) => {
+          return <li key={category.id}>{category.name}</li>;
+        });
+
+        setCategories(<ul className="category">{cats}</ul>);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+  if (width <= 768) return null;
+
+  return <div className="leftmenu">{categories}</div>;
 };
 
 export default LeftMenu;
