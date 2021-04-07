4a96b9dfa0d20ae87cb9dc0c16d2ce38a04aa3de
kaypee <resaba.kaypee@gmail.com>
2021-04-07 13:04:13 +0700

    client-updated to use threadpointsinline component

--- forum-client/src/components/routes/thread/ThreadResponse.tsx
+++ forum-client/src/components/routes/thread/ThreadResponse.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
 import RichEditor from "../../editor/RichEditor";
+import ThreadPointsInline from "../../points/ThreadPointsInline";
 import UserNameAndTime from "./UserNameAndTime";
 
 interface ThreadResponseProps {
@@ -18,6 +19,9 @@ const ThreadResponse: FC<ThreadResponseProps> = ({
     <div>
       <div>
         <UserNameAndTime userName={userName} lastModifiedOn={lastModifiedOn} />
+        <span style={{ marginLeft: "1em" }}>
+          <ThreadPointsInline points={points || 0} />
+        </span>
       </div>
       <div className="thread-body-editor">
         <RichEditor existingBody={body} />
