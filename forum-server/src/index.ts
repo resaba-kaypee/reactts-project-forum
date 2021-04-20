78949f31e65d3d7f22dea14b2bb6f92c72d87b3a
kaypee <resaba.kaypee@gmail.com>
2021-04-20 22:48:06 +0700

    server-init repo layer for postgres and typeorm

--- forum-server/src/index.ts
+++ forum-server/src/index.ts
@@ -3,6 +3,7 @@ import * as dotenv from "dotenv";
 import session from "express-session";
 import connectRedis from "connect-redis";
 import Redis from "ioredis";
+import { createConnection } from "typeorm";
 
 dotenv.config({ path: `${__dirname}/config.env` });
 
@@ -15,52 +16,60 @@ declare module "express-session" {
   }
 }
 
-const app = express();
-const router = express.Router();
+const main = async () => {
+  const app = express();
+  const router = express.Router();
 
-const redis = new Redis({
-  port: Number(process.env.REDIS_PORT),
-  host: process.env.REDIS_HOST,
-  password: process.env.REDIS_PASSWORD,
-  maxRetriesPerRequest: null,
-});
+  await createConnection();
 
-const RedisStore = connectRedis(session);
-const redisStore = new RedisStore({ client: redis });
+  const redis = new Redis({
+    port: Number(process.env.REDIS_PORT),
+    host: process.env.REDIS_HOST,
+    password: process.env.REDIS_PASSWORD,
+    maxRetriesPerRequest: null,
+  });
 
-app.use(
-  session({
-    store: redisStore,
-    name: process.env.COOKIE_NAME,
-    sameSite: "Strict",
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      path: "/",
-      httpOnly: true,
-      secure: false,
-      maxAge: 1000 * 60 * 60 * 24,
-    },
-  } as any)
-);
+  const RedisStore = connectRedis(session);
+  const redisStore = new RedisStore({ client: redis });
 
-app.use(router);
-
-router.get("/", (req, res, next) => {
-  if (!req.session!.userid) {
-    req.session!.userid = req.query.userid;
-    console.log("userid is set");
-    req.session!.loadedCount = 0;
-  } else {
-    req.session!.loadedCount = Number(req.session!.loadedCount) + 1;
-  }
-
-  res.send(
-    `userid: ${req.session!.userid}, loadedCount:: ${req.session!.loadedCount}`
+  app.use(
+    session({
+      store: redisStore,
+      name: process.env.COOKIE_NAME,
+      sameSite: "Strict",
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        path: "/",
+        httpOnly: true,
+        secure: false,
+        maxAge: 1000 * 60 * 60 * 24,
+      },
+    } as any)
   );
-});
 
-app.listen({ port: process.env.SERVER_PORT }, () => {
-  console.log(`Server ready on port ${process.env.SERVER_PORT}`);
-});
+  app.use(router);
+
+  router.get("/", (req, res, next) => {
+    if (!req.session!.userid) {
+      req.session!.userid = req.query.userid;
+      console.log("userid is set");
+      req.session!.loadedCount = 0;
+    } else {
+      req.session!.loadedCount = Number(req.session!.loadedCount) + 1;
+    }
+
+    res.send(
+      `userid: ${req.session!.userid}, loadedCount:: ${
+        req.session!.loadedCount
+      }`
+    );
+  });
+
+  app.listen({ port: process.env.SERVER_PORT }, () => {
+    console.log(`Server ready on port ${process.env.SERVER_PORT}`);
+  });
+};
+
+main();
