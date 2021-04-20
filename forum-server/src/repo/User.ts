665de3d2c2d1e913f92dc83ee8322dd399e3a8af
kaypee <resaba.kaypee@gmail.com>
2021-04-20 22:59:37 +0700

    server-created repo for thread, threaditems and user

---
+++ forum-server/src/repo/User.ts
@@ -0,0 +1,46 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Length } from "class-validator";
+
+@Entity({ name: "Users" })
+export class User {
+  @PrimaryGeneratedColumn({ name: "Id", type: "bigint" })
+  id: string;
+
+  @Column("varchar", {
+    name: "Email",
+    length: 120,
+    unique: true,
+    nullable: false,
+  })
+  email: string;
+
+  @Column("varchar", {
+    name: "UserName",
+    length: 60,
+    unique: true,
+    nullable: false,
+  })
+  userName: string;
+
+  @Column("varchar", {
+    name: "Password",
+    length: 100,
+    nullable: false,
+  })
+  @Length(8, 100)
+  password: string;
+
+  @Column("boolean", {
+    name: "Confirmed",
+    default: false,
+    nullable: false,
+  })
+  confirm: boolean;
+
+  @Column("boolean", {
+    name: "IsDisabled",
+    default: false,
+    nullable: false,
+  })
+  isDisabled: boolean;
+}
