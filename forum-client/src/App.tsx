fbe4f5a4d1e1548dad44c5b16dded5249ef06845
kaypee <resaba.kaypee@gmail.com>
2021-05-11 01:21:27 +0700

    client-prepare getting all catgeries on mobile

--- forum-client/src/App.tsx
+++ forum-client/src/App.tsx
@@ -1,14 +1,28 @@
 import React, { useEffect } from "react";
-import Home from "./components/routes/Home";
 import { Route, Switch } from "react-router";
 import { useDispatch } from "react-redux";
-import Thread from "./components/routes/thread/Thread";
-import "./App.css";
 import { UserProfileSetType } from "./store/user/Reducer";
+import { gql, useQuery } from "@apollo/client";
+import { ThreadCategoriesType } from "./store/categories/Reducer";
+import "./App.css";
+import Home from "./components/routes/Home";
+import Thread from "./components/routes/thread/Thread";
 import UserProfile from "./components/routes/userProfile/UserProfile";
 
+const GetAllCategories = gql`
+  query getAllCategories {
+    getAllCategories {
+      id
+      userName
+    }
+  }
+`;
+
 function App() {
+  const { data: categoriesData } = useQuery(GetAllCategories);
+  // TODOS useRefreshReduxMe
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch({
       type: UserProfileSetType,
@@ -17,7 +31,14 @@ function App() {
         userName: "testUser",
       },
     });
-  }, [dispatch]);
+
+    if (categoriesData && categoriesData.getAllCategories) {
+      dispatch({
+        type: ThreadCategoriesType,
+        payload: categoriesData.getAllCategories,
+      });
+    }
+  }, [dispatch, categoriesData]);
 
   const renderHome = (props: any) => <Home {...props} />;
   const renderThread = (props: any) => <Thread {...props} />;
