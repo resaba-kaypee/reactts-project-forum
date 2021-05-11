0808eaba9d481dd867c0d684fe8c38821351acca
kaypee <resaba.kaypee@gmail.com>
2021-05-11 19:31:25 +0700

    client-use categorydropdown on mobile

--- forum-client/src/components/areas/main/MainHeader.tsx
+++ forum-client/src/components/areas/main/MainHeader.tsx
@@ -1,18 +1,31 @@
 import React, { FC } from "react";
-import Category from "../../../models/Category";
 import "./Main.css";
+import Category from "../../../models/Category";
+import { useWindowDimensions } from "../../../hooks/useWindowDimensions";
+import CategoryDropDown from "../../CategoryDropDown";
 
 interface MainHeaderProps {
   category?: Category;
 }
 
 const MainHeader: FC<MainHeaderProps> = ({ category }) => {
+  const { width } = useWindowDimensions();
+
+  const getLabelElement = () => {
+    if (width <= 768) {
+      return (
+        <CategoryDropDown navigate={true} preselectedCategory={category} />
+      );
+    } else {
+      return <strong>{category?.name || "Placeholder"}</strong>;
+    }
+  };
   return (
     <div className="main-header">
       <div
         className="title-bar"
         style={{ marginBottom: ".25em", paddingBottom: "0" }}>
-        <strong>{category?.name || "Placeholder"}</strong>
+        {getLabelElement()}
       </div>
     </div>
   );
