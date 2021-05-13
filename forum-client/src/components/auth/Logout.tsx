365e0ca6cc015a55c1c24870d032c91e49413ffc
kaypee <resaba.kaypee@gmail.com>
2021-05-13 19:02:45 +0700

    client-implemented logout

--- forum-client/src/components/auth/Logout.tsx
+++ forum-client/src/components/auth/Logout.tsx
@@ -1,12 +1,40 @@
 import React, { FC } from "react";
 import ReactModal from "react-modal";
 import { ModalProps } from "../../types/ModalProps";
+import { gql, useMutation } from "@apollo/client";
 import "./Auth.css";
+import { AppState } from "../../store/AppState";
+import { useSelector } from "react-redux";
+import useRefreshReduxMe, { Me } from "../../hooks/useRefreshReduxMe";
+
+const LogoutMutation = gql`
+  mutation logout($email: String!) {
+    logout(email: $email)
+  }
+`;
 
 const Logout: FC<ModalProps> = ({ isOpen, onClickToggle }) => {
-  const onClickLogin = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  const user = useSelector((state: AppState) => state.user);
+  const [execLogout] = useMutation(LogoutMutation, {
+    refetchQueries: [
+      {
+        query: Me,
+      },
+    ],
+  });
+  const { deleteMe } = useRefreshReduxMe();
+
+  const onClickLogin = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
     e.preventDefault();
     onClickToggle(e);
+
+    await execLogout({
+      variables: { email: user?.email ?? "" },
+    });
+
+    deleteMe();
   };
 
   const onClickCancel = (
@@ -20,7 +48,8 @@ const Logout: FC<ModalProps> = ({ isOpen, onClickToggle }) => {
       className="modal-menu"
       isOpen={isOpen}
       onRequestClose={onClickToggle}
-      shouldCloseOnOverlayClick={true}>
+      shouldCloseOnOverlayClick={true}
+      ariaHideApp={false}>
       <form>
         <div className="logout-inputs">Are you sure you want to logout?</div>
         <div className="form-buttons form-buttons-sm">
