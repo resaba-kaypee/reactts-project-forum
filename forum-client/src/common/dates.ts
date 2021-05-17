38324f414fce3aede50a0caa69a0b45b1ad55f4a
kaypee <resaba.kaypee@gmail.com>
2021-05-17 22:57:24 +0700

    client-convert string to date object and minor changes

--- forum-client/src/common/dates.ts
+++ forum-client/src/common/dates.ts
@@ -1,20 +1,24 @@
-import { format, differenceInMinutes } from "date-fns";
+import {
+  // format,
+  differenceInMinutes,
+  differenceInDays,
+} from "date-fns";
 
-const StandardDateTimeFormat = "M/dd/yyyy";
-
+// const StandardDateTimeFormat = "M/dd/yyyy";
 export const getTimePassIfLessThanDay = (compTime: Date | null): string => {
   if (!compTime) return "";
 
   const now = new Date();
-  const diffInMinutes = differenceInMinutes(now, compTime);
-
-  console.log("diff: ", diffInMinutes);
+  const thisDate = new Date(compTime);
+  const diffInMinutes = differenceInMinutes(now, thisDate);
+  const diffInDays = differenceInDays(now, thisDate);
 
   if (diffInMinutes > 60) {
     if (diffInMinutes > 24 * 60) {
-      return format(compTime, StandardDateTimeFormat);
+      // return format(thisDate, StandardDateTimeFormat)
+      return `${diffInDays} days ago`;
     }
-    return `${Math.round(diffInMinutes / 60)}h ago`;
+    return `${Math.round(diffInMinutes / 60)} h ago`;
   }
-  return `${Math.round(diffInMinutes)}m ago`;
+  return `${Math.round(diffInMinutes)} m ago`;
 };
