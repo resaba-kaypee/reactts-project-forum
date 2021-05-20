b1342c0980e934ece37c164bc69313bbf28d749d
kaypee <resaba.kaypee@gmail.com>
2021-05-20 15:36:01 +0700

    client-able to upvote downvote response

--- forum-client/src/components/points/ThreadPointsInline.tsx
+++ forum-client/src/components/points/ThreadPointsInline.tsx
@@ -1,23 +1,92 @@
 import React, { FC } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
-import { ThreadPointsBarProps } from "./ThreadPointsBar";
+import {
+  faChevronDown,
+  faChevronUp,
+  faHeart,
+} from "@fortawesome/free-solid-svg-icons";
+import { gql, useMutation } from "@apollo/client";
 
-const ThreadPointsInline: FC<ThreadPointsBarProps> = ({
+const UpdateThreadItemPoint = gql`
+  mutation UpdateThreadItemPoint($threadItemId: ID!, $increment: Boolean!) {
+    updateThreadItemPoint(threadItemId: $threadItemId, increment: $increment)
+  }
+`;
+
+class ThreadPointsInlineProps {
+  points: number = 0;
+  userId?: string;
+  threadId?: string;
+  threadItemId?: string;
+  allowUpdatePoints?: boolean = false;
+  refreshThread?: () => void;
+}
+
+const ThreadPointsInline: FC<ThreadPointsInlineProps> = ({
   points,
-  responseCount,
+  userId,
+  threadId,
+  threadItemId,
+  allowUpdatePoints,
+  refreshThread,
 }) => {
+  const [execUpdateThreadItemPoint] = useMutation(UpdateThreadItemPoint);
+
+  const onClickIncThreadItemPoint = async (
+    e: React.MouseEvent<SVGSVGElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    console.log("incremented");
+    await execUpdateThreadItemPoint({
+      variables: {
+        threadItemId,
+        increment: true,
+      },
+    });
+
+    refreshThread && refreshThread();
+  };
+
+  const onClickDecThreadItemPoint = async (
+    e: React.MouseEvent<SVGSVGElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    console.log("decremented");
+    await execUpdateThreadItemPoint({
+      variables: {
+        threadItemId,
+        increment: false,
+      },
+    });
+
+    refreshThread && refreshThread();
+  };
   return (
-    <>
-      <label style={{ marginRight: ".75em", marginTop: ".25em" }}>
-        {points || 0}
+    <span className="threadpointsinline-item">
+      <div
+        className="threadpointsinline-item-btn"
+        style={{ display: `${allowUpdatePoints ? "block" : "none"}` }}>
         <FontAwesomeIcon
-          className="points-icon"
-          icon={faHeart}
-          style={{ marginLeft: ".2em" }}
+          icon={faChevronUp}
+          className="point-icon"
+          onClick={onClickIncThreadItemPoint}
         />
-      </label>
-    </>
+      </div>
+      {points}
+      <div
+        className="threadpointsinline-item-btn"
+        style={{ display: `${allowUpdatePoints ? "block" : "none"}` }}>
+        <FontAwesomeIcon
+          icon={faChevronDown}
+          className="points-icon"
+          onClick={onClickDecThreadItemPoint}
+        />
+      </div>
+
+      <div className="threadpointsinline-item-btn">
+        <FontAwesomeIcon className="points-icon" icon={faHeart} />
+      </div>
+    </span>
   );
 };
 
