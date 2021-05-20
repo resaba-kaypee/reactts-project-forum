8b98bd75b5585848c89c6d85a75c0fc39fe13e8f
kaypee <resaba.kaypee@gmail.com>
2021-05-20 15:35:31 +0700

    client-updated props

--- forum-client/src/components/routes/thread/ThreadResponse.tsx
+++ forum-client/src/components/routes/thread/ThreadResponse.tsx
@@ -6,23 +6,35 @@ import UserNameAndTime from "./UserNameAndTime";
 interface ThreadResponseProps {
   body?: string;
   userName?: string;
+  userId: string;
+  threadItemId: string;
   lastModifiedOn?: Date;
   points: number;
   readOnly: boolean;
+  refreshThread?: () => void;
 }
 const ThreadResponse: FC<ThreadResponseProps> = ({
   body,
   userName,
+  userId,
+  threadItemId,
   lastModifiedOn,
   points,
   readOnly,
+  refreshThread,
 }) => {
   return (
     <div>
       <div>
         <UserNameAndTime userName={userName} lastModifiedOn={lastModifiedOn} />
         <span style={{ marginLeft: "1em" }}>
-          <ThreadPointsInline points={points || 0} />
+          <ThreadPointsInline
+            points={points || 0}
+            userId={userId}
+            threadItemId={threadItemId}
+            allowUpdatePoints={true}
+            refreshThread={refreshThread}
+          />
         </span>
       </div>
       <div className="thread-body-editor">
