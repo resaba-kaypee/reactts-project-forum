be110f0b8f087e64cd2a6aa30ddf49f400d79b34
kaypee <resaba.kaypee@gmail.com>
2021-05-21 11:42:37 +0700

    client-created updateThreadPoint hook

---
+++ forum-client/src/hooks/useUpdateThreadPoint.ts
@@ -0,0 +1,50 @@
+import { gql, useMutation } from "@apollo/client";
+
+const UpdateThreadPoint = gql`
+  mutation UpdateThreadItemPoint($threadId: ID!, $increment: Boolean!) {
+    updateThreadPoint(threadId: $threadId, increment: $increment)
+  }
+`;
+
+const useUpdateThreadPoint = (
+  refreshThread?: () => void,
+  threadId?: string
+) => {
+  const [execUpdateThreadPoint] = useMutation(UpdateThreadPoint);
+
+  const onClickIncThreadPoint = async (
+    e: React.MouseEvent<SVGSVGElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+
+    await execUpdateThreadPoint({
+      variables: {
+        threadId,
+        increment: true,
+      },
+    });
+
+    refreshThread && refreshThread();
+  };
+
+  const onClickDecThreadPoint = async (
+    e: React.MouseEvent<SVGSVGElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+
+    await execUpdateThreadPoint({
+      variables: {
+        threadId,
+        increment: false,
+      },
+    });
+    refreshThread && refreshThread();
+  };
+
+  return {
+    onClickDecThreadPoint,
+    onClickIncThreadPoint,
+  };
+};
+
+export default useUpdateThreadPoint;
