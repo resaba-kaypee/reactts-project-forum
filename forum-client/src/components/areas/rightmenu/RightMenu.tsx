efcd7c799f9623693f89c81e199ed46620efa29d
kaypee <resaba.kaypee@gmail.com>
2021-05-25 22:54:08 +0700

    client-able to view top categories

--- forum-client/src/components/areas/rightmenu/RightMenu.tsx
+++ forum-client/src/components/areas/rightmenu/RightMenu.tsx
@@ -1,31 +1,46 @@
 import React, { useState, useEffect } from "react";
 import { useWindowDimensions } from "../../../hooks/useWindowDimensions";
-import { getTopCategories } from "../../../services/DataService";
 import TopCategory from "./TopCategory";
 import groupBy from "lodash/groupBy";
 import "./RightMenu.css";
+import { gql, useQuery } from "@apollo/client";
+
+const GetTopCategories = gql`
+  query getTopCategories {
+    getTopCategories {
+      threadId
+      categoryId
+      categoryName
+      title
+    }
+  }
+`;
 
 const RightMenu = () => {
+  const { data: categoryThreadData } = useQuery(GetTopCategories);
+
   const { width } = useWindowDimensions();
-  const [topCategories, setTopCategories] = useState<
-    Array<JSX.Element> | undefined
-  >();
+  const [topCategories, setTopCategories] =
+    useState<Array<JSX.Element> | undefined>();
 
   useEffect(() => {
-    getTopCategories().then((res) => {
-      const topCatThreads = groupBy(res, "category");
-      const topElements = [];
+    if (categoryThreadData && categoryThreadData.getTopCategories) {
+      const topCatThreads = groupBy(
+        categoryThreadData.getTopCategories,
+        "categoryName"
+      );
 
+      const topElements = [];
       for (let key in topCatThreads) {
         const currentTop = topCatThreads[key];
         topElements.push(<TopCategory key={key} topCategories={currentTop} />);
       }
-
       setTopCategories(topElements);
-    });
-  });
+    }
+  }, [categoryThreadData]);
 
   if (width <= 768) return null;
+
   return <div className="rightmenu rightmenu-container">{topCategories}</div>;
 };
 
