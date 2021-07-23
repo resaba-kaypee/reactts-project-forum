40fa6a9dea0880bf5041ba2fb7c80bcd8ed28613
kaypee <resaba.kaypee@gmail.com>
2021-07-24 01:45:06 +0800

    client-added redux-dev-tools for bedugging state

--- forum-client/src/store/configureStore.ts
+++ forum-client/src/store/configureStore.ts
@@ -1,8 +1,9 @@
+import { composeWithDevTools } from "redux-devtools-extension";
 import { createStore } from "redux";
 import { rootReducer } from "./AppState";
 
 const configureStore = () => {
-  return createStore(rootReducer, {});
+  return createStore(rootReducer, {}, composeWithDevTools());
 };
 
 export default configureStore;
