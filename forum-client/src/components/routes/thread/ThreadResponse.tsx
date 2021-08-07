f8bb7b254d7fde70988d618f69db49fcd93de858
kaypee <resaba.kaypee@gmail.com>
2021-08-07 12:10:59 +0800

    client-moved get thread items from thread.tsx

--- forum-client/src/components/routes/thread/ThreadResponse.tsx
+++ forum-client/src/components/routes/thread/ThreadResponse.tsx
@@ -1,19 +1,7 @@
-import { gql, useMutation } from "@apollo/client";
-import React, { FC, useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { Node } from "slate";
-import { AppState } from "../../../store/AppState";
+import React, { FC } from "react";
 import RichEditor from "../../editor/RichEditor";
 import ThreadPointsInline from "../../points/ThreadPointsInline";
 import UserNameAndTime from "./UserNameAndTime";
-
-const CreateThreadItem = gql`
-  mutation createThreadItem($threadId: ID!, $body: String!) {
-    createThreadItem(threadId: $threadId, body: $body) {
-      messages
-    }
-  }
-`;
 
 interface ThreadResponseProps {
   body?: string;
@@ -23,7 +11,6 @@ interface ThreadResponseProps {
   points: number;
   lastModifiedOn?: Date;
   readOnly: boolean;
-  refreshThread?: () => void;
 }
 const ThreadResponse: FC<ThreadResponseProps> = ({
   body,
@@ -33,50 +20,7 @@ const ThreadResponse: FC<ThreadResponseProps> = ({
   points,
   lastModifiedOn,
   readOnly,
-  refreshThread,
 }) => {
-  const user = useSelector((state: AppState) => state.user);
-  const [execCreateThreadItem] = useMutation(CreateThreadItem);
-  const [postMsg, setPostMsg] = useState("");
-  const [bodyToSave, setBodyToSave] = useState("");
-
-  useEffect(() => {
-    if (body) {
-      setBodyToSave(body || "");
-    }
-  }, [body]);
-
-  const onClickPost = async (
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
-    e.preventDefault();
-
-    if (!user) {
-      setPostMsg("Please login before adding a response.");
-    } else if (!threadId) {
-      setPostMsg("A parent thread must exist before a response can be posted.");
-    } else if (!bodyToSave) {
-      setPostMsg("Please enter some text.");
-    } else {
-      await execCreateThreadItem({
-        variables: {
-          userId: user ? user.id : "0",
-          threadId,
-          body: bodyToSave,
-        },
-      });
-
-      refreshThread && refreshThread();
-    }
-  };
-
-  const receiveBody = (body: Node[]) => {
-    const newBody = JSON.stringify(body);
-    if (bodyToSave !== newBody) {
-      setBodyToSave(newBody);
-    }
-  };
-
   return (
     <div>
       <div>
@@ -92,28 +36,14 @@ const ThreadResponse: FC<ThreadResponseProps> = ({
                 pointsFromThread={points}
                 threadItemId={threadItemId}
                 allowUpdatePoints={true}
-                refreshThread={refreshThread}
               />
             </span>
           ) : null}
         </div>
-        {!readOnly && threadId ? (
-          <div style={{ marginTop: ".5em" }}>
-            <button className="action-btn" onClick={onClickPost}>
-              Post Response
-            </button>
-          </div>
-        ) : null}
       </div>
       <div className="thread-body-editor">
-        <RichEditor
-          existingBody={bodyToSave}
-          readOnly={readOnly}
-          sendOutBody={receiveBody}
-        />
+        <RichEditor existingBody={body} readOnly={readOnly} />
       </div>
-      {/* NOTES:  Error Message*/}
-      <strong>{postMsg}</strong>
     </div>
   );
 };
--- forum-client/src/components/routes/thread/ThreadResponseBuilder.tsx
+++ forum-client/src/components/routes/thread/ThreadResponseBuilder.tsx
@@ -1,46 +1,102 @@
 import React, { FC, useEffect, useState } from "react";
+import { gql, useLazyQuery } from "@apollo/client";
 import ThreadItem from "../../../models/ThreadItem";
 import ThreadResponse from "./ThreadResponse";
 
+const GetThreadItemsByThreadId = gql`
+  query getThreadItemsByThreadId($threadId: ID!) {
+    getThreadItemsByThreadId(threadId: $threadId) {
+      ... on EntityResult {
+        messages
+      }
+
+      ... on ThreadItemArray {
+        threadItems {
+          id
+          body
+          points
+          lastModifiedOn
+          user {
+            id
+            userName
+          }
+        }
+      }
+    }
+  }
+`;
+
 interface ThreadResponseBuilderProps {
-  threadItems?: Array<ThreadItem>;
+  threadId?: string;
   readOnly: boolean;
-  refreshThread: () => void;
 }
+
 const ThreadResponseBuilder: FC<ThreadResponseBuilderProps> = ({
-  threadItems,
+  threadId,
   readOnly,
-  refreshThread,
 }) => {
+  const [
+    execGetThreadItemsByThreadId,
+    {
+      // called,
+      // error,
+      data: threadItemData,
+    },
+  ] = useLazyQuery(GetThreadItemsByThreadId, { fetchPolicy: "no-cache" });
+
   const [responseElements, setResponseElements] = useState<
     JSX.Element | undefined
   >();
 
   useEffect(() => {
-    if (threadItems) {
-      const thResponses = threadItems.map((ti) => {
-        return (
-          <li key={`thr-${ti.id}`}>
-            <ThreadResponse
-              body={ti.body}
-              userName={ti.user.userName}
-              lastModifiedOn={ti.createdOn}
-              points={ti.points}
-              readOnly={readOnly}
-              threadItemId={ti?.id || "0"}
-              refreshThread={refreshThread}
-            />
-          </li>
-        );
+    if (threadId) {
+      execGetThreadItemsByThreadId({
+        variables: { threadId },
       });
-      setResponseElements(<ul>{thResponses}</ul>);
+    }
+  }, [threadId, execGetThreadItemsByThreadId]);
+
+  useEffect(() => {
+    if (
+      threadItemData &&
+      threadItemData.getThreadItemsByThreadId &&
+      threadItemData.getThreadItemsByThreadId.threadItems
+    ) {
+      const thResponses =
+        threadItemData.getThreadItemsByThreadId.threadItems.map(
+          (ti: ThreadItem) => {
+            return (
+              <li key={`thr-${ti.id}`}>
+                <ThreadResponse
+                  body={ti.body}
+                  userName={ti.user.userName}
+                  lastModifiedOn={ti.createdOn}
+                  points={ti.points}
+                  readOnly={readOnly}
+                  threadItemId={ti?.id || "0"}
+                />
+              </li>
+            );
+          }
+        );
+      setResponseElements(
+        threadItemData.getThreadItemsByThreadId.threadItems.length > 0 ? (
+          <ul>{thResponses}</ul>
+        ) : (
+          <ul>
+            <li>
+              <p>No reply to this topic yet.</p>
+            </li>
+          </ul>
+        )
+      );
     }
     // eslint-disable-next-line
-  }, [threadItems, readOnly]);
+  }, [threadItemData, readOnly]);
+
   return (
     <div className="thread-body-container">
       <strong style={{ marginBottom: ".75em" }}>Responses</strong>
-      {threadItems?.length === 0 ? <p>No reply to this topic yet.</p> : null}
       {responseElements}
     </div>
   );
