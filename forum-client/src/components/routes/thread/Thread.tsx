c2e8d301f36d059896dffb253dbc98a48fdea8a0
kaypee <resaba.kaypee@gmail.com>
2021-08-07 12:11:24 +0800

    client-move create thread from thread.tsx

--- forum-client/src/components/routes/thread/Thread.tsx
+++ forum-client/src/components/routes/thread/Thread.tsx
@@ -1,13 +1,10 @@
-import React, { useEffect, useReducer, useState } from "react";
-import { gql, useLazyQuery, useMutation } from "@apollo/client";
+import React, { useEffect, useState } from "react";
+import { gql, useLazyQuery } from "@apollo/client";
 import { useSelector } from "react-redux";
-import { useHistory, useParams } from "react-router-dom";
-import { Node } from "slate";
+import { useParams } from "react-router-dom";
 import { AppState } from "../../../store/AppState";
-import { getTextFromNodes } from "../../editor/RichEditor";
 import "./Thread.css";
 import Nav from "../../areas/Nav";
-import Category from "../../../models/Category";
 import ThreadModel from "../../../models/Thread";
 import ThreadContent from "./ThreadContent";
 import ThreadPostResponse from "./ThreadPostResponse";
@@ -48,33 +45,7 @@ const GetThreadById = gql`
   }
 `;
 
-const CreateThread = gql`
-  mutation createThread($categoryId: ID!, $title: String!, $body: String!) {
-    createThread(categoryId: $categoryId, title: $title, body: $body) {
-      messages
-    }
-  }
-`;
-
-const threadReducer = (state: any, action: any) => {
-  switch (action.type) {
-    case "userId":
-      return { ...state, userId: action.payload };
-    case "category":
-      return { ...state, category: action.payload };
-    case "title":
-      return { ...state, title: action.payload };
-    case "body":
-      return { ...state, body: action.payload };
-    case "bodyNode":
-      return { ...state, bodyNode: action.payload };
-    default:
-      throw new Error("Unknown action type");
-  }
-};
-
 const Thread = () => {
-  const [execCreateThread] = useMutation(CreateThread);
   const [
     execGetThreadById,
     {
@@ -88,16 +59,6 @@ const Thread = () => {
   const [readOnly, setReadOnly] = useState(false);
   const { id } = useParams<{ id: any }>();
   const user = useSelector((state: AppState) => state.user);
-  const [{ userId, category, title, bodyNode }, threadReducerDispatch] =
-    useReducer(threadReducer, {
-      userId: user ? user.id : "0",
-      category: undefined,
-      title: "",
-      body: "",
-      bodyNode: undefined,
-    });
-  const [postMsg, setPostMsg] = useState("");
-  const history = useHistory();
 
   const refreshThread = () => {
     //   console.log("refreshed");
@@ -122,13 +83,6 @@ const Thread = () => {
   }, [id, execGetThreadById]);
 
   useEffect(() => {
-    threadReducerDispatch({
-      type: "userId",
-      payload: user ? user.id : "0",
-    });
-  }, [user]);
-
-  useEffect(() => {
     if (threadData && threadData.getThreadById) {
       setThread(threadData.getThreadById);
       setReadOnly(true);
@@ -139,94 +93,16 @@ const Thread = () => {
     // eslint-disable-next-line
   }, [threadData]);
 
-  const receiveSelectedCategory = (cat: Category) => {
-    threadReducerDispatch({
-      type: "category",
-      payload: cat,
-    });
-  };
-
-  const receiveTitle = (updatedTitle: string) => {
-    threadReducerDispatch({
-      type: "title",
-      payload: updatedTitle,
-    });
-  };
-
-  const receiveBody = (body: Node[]) => {
-    threadReducerDispatch({
-      type: "bodyNode",
-      payload: body,
-    });
-    threadReducerDispatch({
-      type: "body",
-      payload: getTextFromNodes(body),
-    });
-  };
-
-  const onClickPost = async (
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
-    e.preventDefault();
-
-    console.log("bodyNode: ", getTextFromNodes(bodyNode));
-    if (!userId || userId === "0") {
-      setPostMsg("You must be logged in before you can post.");
-    } else if (!category) {
-      setPostMsg("Please select a category for your post.");
-    } else if (!title) {
-      setPostMsg("Please enter a title.");
-    } else if (!bodyNode) {
-      setPostMsg("Please enter a body.");
-    } else {
-      setPostMsg("");
-
-      const newThread = {
-        userId,
-        categoryId: category?.id,
-        title,
-        body: JSON.stringify(bodyNode),
-      };
-
-      const { data: createThreadMsg } = await execCreateThread({
-        variables: newThread,
-      });
-
-      if (
-        createThreadMsg.createThread &&
-        createThreadMsg.createThread.messages &&
-        !isNaN(createThreadMsg.createThread.messages[0])
-      ) {
-        setPostMsg("Thread posted successfully.");
-        history.push(`/thread/${createThreadMsg.createThread.messages[0]}`);
-      } else {
-        setPostMsg(createThreadMsg.createThread.messages[0]);
-      }
-    }
-  };
   return (
     <div className="screen-root-container">
       <div className="thread-nav-container">
         <Nav />
       </div>
-      <ThreadContent
-        id={id}
-        onClickPost={onClickPost}
-        postMsg={postMsg}
-        readOnly={readOnly}
-        receiveSelectedCategory={receiveSelectedCategory}
-        receiveTitle={receiveTitle}
-        receiveBody={receiveBody}
-        thread={thread}
-      />
+      <ThreadContent readOnly={readOnly} thread={thread} />
       {thread ? (
         <div className="thread-content-response-container">
           <hr className="thread-section-divider" />
-          <ThreadResponseBuilder
-            threadItems={thread?.threadItems}
-            readOnly={readOnly}
-            refreshThread={refreshThread}
-          />
+          <ThreadResponseBuilder threadId={thread?.id} readOnly={readOnly} />
         </div>
       ) : null}
       {thread ? (
--- forum-client/src/components/routes/thread/ThreadContent.tsx
+++ forum-client/src/components/routes/thread/ThreadContent.tsx
@@ -1,7 +1,10 @@
-import React, { FC, useEffect, useState } from "react";
+import React, { FC, useEffect, useState, useReducer } from "react";
+import { useHistory, useParams } from "react-router-dom";
 import { Node } from "slate";
+import { gql, useMutation } from "@apollo/client";
 import { AppState } from "../../../store/AppState";
 import { useSelector } from "react-redux";
+import { getTextFromNodes } from "../../editor/RichEditor";
 import Thread from "../../../models/Thread";
 import ThreadPointsBar from "../../points/ThreadPointsBar";
 import ThreadBody from "./ThreadBody";
@@ -10,32 +13,54 @@ import ThreadHeader from "./ThreadHeader";
 import ThreadTitle from "./ThreadTitle";
 import Category from "../../../models/Category";
 
+const CreateThread = gql`
+  mutation createThread($categoryId: ID!, $title: String!, $body: String!) {
+    createThread(categoryId: $categoryId, title: $title, body: $body) {
+      messages
+    }
+  }
+`;
+
+const threadReducer = (state: any, action: any) => {
+  switch (action.type) {
+    case "userId":
+      return { ...state, userId: action.payload };
+    case "category":
+      return { ...state, category: action.payload };
+    case "title":
+      return { ...state, title: action.payload };
+    case "body":
+      return { ...state, body: action.payload };
+    case "bodyNode":
+      return { ...state, bodyNode: action.payload };
+    default:
+      throw new Error("Unknown action type");
+  }
+};
+
 interface ThreadContentProps {
-  id: string;
-  onClickPost?: (
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => Promise<void>;
-  postMsg: string;
   readOnly: boolean;
-  receiveSelectedCategory: (cat: Category) => void;
-  receiveTitle: (updatedTitle: string) => void;
-  receiveBody: (body: Node[]) => void;
   thread?: Thread;
 }
 
-const ThreadContent: FC<ThreadContentProps> = ({
-  id,
-  onClickPost,
-  postMsg,
-  readOnly,
-  receiveSelectedCategory,
-  receiveTitle,
-  receiveBody,
-  thread,
-}) => {
+const ThreadContent: FC<ThreadContentProps> = ({ readOnly, thread }) => {
   const message = useSelector((state: AppState) => state.message);
   const user = useSelector((state: AppState) => state.user);
+  const { id } = useParams<{ id: any }>();
   const [msg, setMsg] = useState<string>("");
+  const [postMsg, setPostMsg] = useState("");
+  const history = useHistory();
+
+  const [execCreateThread] = useMutation(CreateThread);
+
+  const [{ userId, category, title, bodyNode }, threadReducerDispatch] =
+    useReducer(threadReducer, {
+      userId: user ? user.id : "0",
+      category: undefined,
+      title: "",
+      body: "",
+      bodyNode: undefined,
+    });
 
   useEffect(() => {
     if (message) {
@@ -50,18 +75,90 @@ const ThreadContent: FC<ThreadContentProps> = ({
   }, [user]);
 
   useEffect(() => {
+    let timeOut: any;
     if (msg) {
-      const timeOut = setTimeout(() => setMsg(""), 3000);
-
-      return () => clearTimeout(timeOut);
+      timeOut = setTimeout(() => setMsg(""), 3000);
     }
+    return () => clearTimeout(timeOut);
   }, [msg]);
+
+  useEffect(() => {
+    threadReducerDispatch({
+      type: "userId",
+      payload: user ? user.id : "0",
+    });
+  }, [user]);
+
+  const receiveSelectedCategory = (cat: Category) => {
+    threadReducerDispatch({
+      type: "category",
+      payload: cat,
+    });
+  };
+
+  const receiveTitle = (updatedTitle: string) => {
+    threadReducerDispatch({
+      type: "title",
+      payload: updatedTitle,
+    });
+  };
+
+  const receiveBody = (body: Node[]) => {
+    threadReducerDispatch({
+      type: "bodyNode",
+      payload: body,
+    });
+    threadReducerDispatch({
+      type: "body",
+      payload: getTextFromNodes(body),
+    });
+  };
+
+  const onClickPost = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+
+    if (!userId || userId === "0") {
+      setPostMsg("You must be logged in before you can post.");
+    } else if (!category) {
+      setPostMsg("Please select a category for your post.");
+    } else if (!title) {
+      setPostMsg("Please enter a title.");
+    } else if (!bodyNode) {
+      setPostMsg("Please enter a body.");
+    } else {
+      setPostMsg("");
+
+      const newThread = {
+        userId,
+        categoryId: category?.id,
+        title,
+        body: JSON.stringify(bodyNode),
+      };
+
+      const { data: createThreadMsg } = await execCreateThread({
+        variables: newThread,
+      });
+
+      if (
+        createThreadMsg.createThread &&
+        createThreadMsg.createThread.messages &&
+        !isNaN(createThreadMsg.createThread.messages[0])
+      ) {
+        setPostMsg("Thread posted successfully.");
+        history.push(`/thread/${createThreadMsg.createThread.messages[0]}`);
+      } else {
+        setPostMsg(createThreadMsg.createThread.messages[0]);
+      }
+    }
+  };
 
   return (
     <div className="thread-content-container">
       <div className="thread-content-post-container">
         <ThreadHeader
-          userName={thread && thread?.user.userName}
+          userName={thread ? thread?.user.userName : user?.userName}
           lastModifiedOn={thread ? thread.lastModifiedOn : new Date()}
           title={thread && thread.title}
         />
@@ -91,7 +188,11 @@ const ThreadContent: FC<ThreadContentProps> = ({
                 Post
               </button>
             </div>
-            <strong>{postMsg}</strong>
+            {postMsg ? (
+              <div className="warning-container">
+                <span className="warning">{postMsg}</span>
+              </div>
+            ) : null}
           </>
         )}
       </div>
@@ -100,8 +201,8 @@ const ThreadContent: FC<ThreadContentProps> = ({
           <span className="warning">{msg}</span>
         </div>
       ) : null}
-      <div className="thread-content-points-container">
-        {thread ? (
+      {thread && id ? (
+        <div className="thread-content-points-container">
           <ThreadPointsBar
             pointsFromThread={thread.points}
             threadId={thread?.id}
@@ -110,8 +211,8 @@ const ThreadContent: FC<ThreadContentProps> = ({
             }
             allowUpdatedPoints={true}
           />
-        ) : null}
-      </div>
+        </div>
+      ) : null}
     </div>
   );
 };
